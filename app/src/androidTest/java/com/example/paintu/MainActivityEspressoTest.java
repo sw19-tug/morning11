69be4d763fdd200ee4563345246da28b8454e628
Srđan Džombić <srdjan.dzombic@wowusluge.com>
2019-03-27 12:26:26 +0100

    PAINT_001 Implements tests for DrawingView [SD, MG]

--- app/src/androidTest/java/com/example/paintu/MainActivityEspressoTest.java
+++ app/src/androidTest/java/com/example/paintu/MainActivityEspressoTest.java
@@ -1,17 +1,25 @@
 package com.example.paintu;
 
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+
+import junit.framework.AssertionFailedError;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.service.autofill.Validators.not;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityEspressoTest {
@@ -29,4 +37,59 @@ public class MainActivityEspressoTest {
 
         onView(withId(R.id.bottom_sheet)).check(matches(isDisplayed()));
     }
+
+    @Test
+    public void drawingViewVisible() {
+        onView(withId(R.id.drawing_view)).check(matches(isDisplayed()));
+        assertNotNull(mainActivityTestRule.getActivity().drawingView);
+    }
+
+    @Test
+    public void bitmapExists() {
+        assertNotNull(mainActivityTestRule.getActivity().drawingView.bitmap);
+    }
+
+    @Test
+    public void canvasExists() {
+        assertNotNull(mainActivityTestRule.getActivity().drawingView.canvas);
+    }
+
+    @Test
+    public void testPointButton() {
+        onView(withId(R.id.tool_point)).perform(click());
+        try {
+            onView(withId(R.id.drawing_view)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        } catch (AssertionFailedError e) {
+            // View not displayed
+            assertEquals(mainActivityTestRule.getActivity().drawingView.tool, 1);
+        }
+
+    }
+
+    @Test
+    public void testLineButton() {
+        onView(withId(R.id.tool_line)).perform(click());
+        try {
+            onView(withId(R.id.drawing_view)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        } catch (AssertionFailedError e) {
+            // View not displayed
+            assertEquals(mainActivityTestRule.getActivity().drawingView.tool, 2);
+
+        }
+
+    }
+
+    @Test
+    public void testPathButton() {
+        onView(withId(R.id.tool_path)).perform(click());
+        try {
+            onView(withId(R.id.drawing_view)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        } catch (AssertionFailedError e) {
+            // View not displayed
+            assertEquals(mainActivityTestRule.getActivity().drawingView.tool, 3);
+        }
+
+    }
+
+
 }
