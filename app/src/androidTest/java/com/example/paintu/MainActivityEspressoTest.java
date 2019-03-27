5f7b2c41f8a7d2013f51f537b922c5ebf5ddc91e
Milos Globocki <milos.globocki@student.tugraz.at>
2019-03-27 09:09:17 +0100

    PAINT_001 Implements espresso tests for image button and bottom sheet [SD, MG]

---
+++ app/src/androidTest/java/com/example/paintu/MainActivityEspressoTest.java
@@ -0,0 +1,32 @@
+package com.example.paintu;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityEspressoTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testButtonVisible() {
+        onView(R.id.bt_tools_chooser).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void bottomSheetVisible() {
+        onView(withId(R.id.bt_tool_chooser).perform(click()));
+
+        onView(R.id.bottom_sheet).check(matches(isDisplayed()));
+    }
+}
