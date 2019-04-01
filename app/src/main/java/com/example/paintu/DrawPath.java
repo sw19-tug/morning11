2f7d8fb65422b5784a6a8a84c8d0dcaade3a2eb6
cernoel <1579434+cernoel@users.noreply.github.com>
2019-04-01 22:11:37 +0200

    PAINT_004 added additional checks [BB,MG]

--- app/src/main/java/com/example/paintu/DrawPath.java
+++ app/src/main/java/com/example/paintu/DrawPath.java
@@ -9,7 +9,7 @@ public class DrawPath {
 
     private Paint paint = null;
     private Canvas canvas = null;
-    private Path path = null;
+    private Path path = new Path();
     private MotionEvent motionEvent = null;
 
     public DrawPath(Canvas canvas, Paint paint)
@@ -20,23 +20,38 @@ public class DrawPath {
 
     public boolean draw(MotionEvent event)
     {
+
+        if(event == null)
+            return false;
+
+        if(canvas == null)
+            return false;
+
         int action = event.getAction();
         float touchX = event.getX();
         float touchY = event.getY();
 
-        if(path == null)
-            path = new Path();
-
         switch (action){
             case MotionEvent.ACTION_DOWN:
+                if(path != null)
+                    path = new Path();
+
+                path.reset();
+
                 path.moveTo(touchX, touchY);
                 path.lineTo(touchX, touchY);
                 break;
             case MotionEvent.ACTION_MOVE:
+                if(path == null)
+                    break;
+
                 path.lineTo(touchX, touchY);
                 canvas.drawPath(path, paint);
                 break;
             case MotionEvent.ACTION_UP:
+                if(path == null)
+                    break;
+
                 path.lineTo(touchX, touchY);
                 canvas.drawPath(path, paint);
                 path.reset();
